a7a00fb0ddd885261556f0be4c65bcaf5bbbcd4f
ReallyFatYoshi <contact.reallyfatyoshi@gmail.com>
2024-01-04 00:12:42 +0100

    Updated readme.md file.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import fs from "node:fs";
 import path from "node:path";
 
+import cors from "express-cors";
 import express from "express";
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(() => resolve(), ms));
+}
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -13,7 +18,8 @@ const IMAGE_PATH = path.join(BASE_PATH, "images");
 const quotes = JSON.parse(fs.readFileSync(QUOTE_PATH));
 const pictures = fs.readdirSync(path.join(IMAGE_PATH));
 
-app.use(express.static('resources/images'))
+app.use(express.static("resources/images"));
+app.use(cors());
 
 app.get("/api/random", (_, res) => {
   res.json({ message: quotes[~~(Math.random() * quotes.length)] });
@@ -22,6 +28,11 @@ app.get("/api/random", (_, res) => {
 app.get("/api/cat", (request, res) => {
   const picture = pictures[~~(Math.random() * pictures.length)];
   res.send({ image: request.url + picture });
+});
+
+app.get("/api/rocket/async", async (_, res) => {
+  await sleep(5000);
+  res.send({ message: "Hello, world! 🚀" });
 });
 
 app.all("*", (_, res) => {
